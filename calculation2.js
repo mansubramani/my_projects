09b17d5617eb31050f93d380c380133c44d80263
Mano <manosubramani4395@gmail.com>
2026-06-09 20:16:15 +0530

    calculation2

---
+++ calculation2.js
@@ -0,0 +1,9 @@
+function calculator(a, b) {
+  console.log("Numbers: " + a + " and " + b);
+  console.log("Addition: " + (a + b));
+  console.log("Subtraction: " + (a - b));
+  console.log("Multiplication: " + (a * b));
+  console.log("Division: " + (a / b));
+}
+
+calculator(10, 5);
